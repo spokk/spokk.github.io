34ff5c01c2a61caa88a1ae336aa994ccfa7b2845
spokk <game1life1@gmail.com>
2017-10-22 01:25:55 +0300

    222
    2222

--- thebrat/script/script.js
+++ thebrat/script/script.js
@@ -2,6 +2,7 @@ var videoMute = document.querySelector(".audio-control");
 var video = document.querySelector("video");
 if (isTouchDevice = !!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/)) {
     document.querySelector("video source").setAttribute("src", "video/intro480.mp4");
+    disableMute();
 }
 videoMute.addEventListener("click", checkMute);
 
