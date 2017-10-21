4259816313b1c10a80dfc26559f1cc315be64871
spokk <game1life1@gmail.com>
2017-10-22 01:46:15 +0300

    111
    1111

---
+++ test/script/script.js
@@ -0,0 +1,25 @@
+var videoMute = document.querySelector(".audio-control");
+var video = document.querySelector("video");
+/*if (isTouchDevice = !!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/)) {
+    document.querySelector("video source").setAttribute("src", "video/intro480.mp4");
+    disableMute();
+}*/
+videoMute.addEventListener("click", checkMute);
+
+function enableMute() {
+    video.muted = false;
+    document.querySelector(".audio-control img").setAttribute("src", "img/audio-control-on.png");
+}
+
+function disableMute() {
+    video.muted = true;
+    document.querySelector(".audio-control img").setAttribute("src", "img/audio-control-off.png");
+}
+
+function checkMute() {
+    if (video.muted == false) {
+        disableMute();
+    } else {
+        enableMute();
+    }
+}
