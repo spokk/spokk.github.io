958226ebabd3551a62d14afe65cd51b5e362fb67
spokk <game1life1@gmail.com>
2017-12-10 20:31:06 +0200

    add page

---
+++ animation/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    /*var controller = new ScrollMagic.Controller();
+
+    var ourScene = new ScrollMagic.Scene({
+            triggerElement: '#a1'
+        })
+        .setClassToggle('#a1', 'qwe')
+        .addIndicators()
+        .addTo(controller);*/
+    $('.fullpage').fullpage({
+        /*sectionsColor:['pink','lightblue','lightyellow','lightgreen'],*/
+        parallax:true
+    });
+});
