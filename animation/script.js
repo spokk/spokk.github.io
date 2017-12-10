1c3532dfb2684b7d419590211bc9fe55b71a820a
spokk <game1life1@gmail.com>
2017-12-10 23:09:55 +0200

    update animation

--- animation/script.js
+++ animation/script.js
@@ -1,14 +1,83 @@
 $(document).ready(function () {
-    /*var controller = new ScrollMagic.Controller();
 
-    var ourScene = new ScrollMagic.Scene({
-            triggerElement: '#a1'
-        })
-        .setClassToggle('#a1', 'qwe')
-        .addIndicators()
-        .addTo(controller);*/
     $('.fullpage').fullpage({
         /*sectionsColor:['pink','lightblue','lightyellow','lightgreen'],*/
-        parallax:true
+        css3: true
     });
+
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: window.innerHeight,
+            triggerHook: 0.9
+        }
+    });
+
+
+
+    new ScrollMagic.Scene({
+            triggerElement: '#sec1'
+        })
+        .setClassToggle("#btn", "bottomLeft") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '#sec1'
+        })
+        .setClassToggle("#btn2", "bottomRight") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+
+
+
+    new ScrollMagic.Scene({
+            triggerElement: "#sec2"
+        })
+        .setClassToggle("#btn", "bottomLeftRevert") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: "#sec2"
+        })
+        .setClassToggle("#btn2", "bottomRightRevert") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+    new ScrollMagic.Scene({
+            triggerElement: '#sec3'
+        })
+        .setClassToggle("#btn", "bottomLeft") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '#sec3'
+        })
+        .setClassToggle("#btn2", "bottomRight") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+
+    new ScrollMagic.Scene({
+            triggerElement: "#sec4"
+        })
+        .setClassToggle("#btn", "bottomLeftRevert") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: "#sec4"
+        })
+        .setClassToggle("#btn2", "bottomRightRevert") // add class toggle
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+
+
 });
