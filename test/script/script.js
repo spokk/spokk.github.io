9d138c829fc477418f9b64e56613915672f9f15a
spokk <game1life1@gmail.com>
2018-01-31 21:31:51 +0200

    delete test, add kuyalnik to test

--- test/script/script.js
+++ test/script/script.js
@@ -1,25 +1,32 @@
-var videoMute = document.querySelector(".audio-control");
-var video = document.querySelector("video");
-/*if (isTouchDevice = !!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/)) {
-    document.querySelector("video source").setAttribute("src", "video/intro480.mp4");
-    disableMute();
-}*/
-videoMute.addEventListener("click", checkMute);
-
-function enableMute() {
-    video.muted = false;
-    document.querySelector(".audio-control img").setAttribute("src", "img/audio-control-on.png");
-}
-
-function disableMute() {
-    video.muted = true;
-    document.querySelector(".audio-control img").setAttribute("src", "img/audio-control-off.png");
-}
-
-function checkMute() {
-    if (video.muted == false) {
-        disableMute();
-    } else {
-        enableMute();
-    }
-}
+$(document).ready(function () {
+    $('.fullpage').fullpage({
+        sectionsColor: ['red', 'green', 'blue', 'yellow'],
+        //        anchors: ['first', 'second', 'third', 'fourth'],
+        onLeave: function (link, index) {
+            if (index == 1) {
+                $('.bottle-block').css({
+                    'transform': 'translateX(215%) translateY(0%) scale(3)'
+                });
+            } else if (index == 2) {
+                $('.bottle-block').css({
+                    'transform': 'translateX(190%) translateY(-20%) scale(1.2)',
+                });
+            } else if (index == 3) {
+                $('.bottle-block').css({
+                    'transform': 'translateX(80%) translateY(0%) scale(1.2)'
+                });
+            } else if (index == 4) {
+                $('.bottle-block').css({
+                    'transform': 'translateX(130%) translateY(80%) scale(1)'
+                });
+            }
+        },
+        //        afterLoad: function (link,index) {
+        //            if (index == 1) {
+        //                $('.bottle-block').css({'right':'0px'});
+        //            } else if (index == 2) {
+        //                $('.bottle-block').css({'right':'50px'});
+        //            }
+        //        }
+    });
+});
