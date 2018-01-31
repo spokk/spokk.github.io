090d609b266c13f7e4f90e227579403379130bad
spokk <game1life1@gmail.com>
2018-01-31 23:48:54 +0200

    some tests

--- kuy/script/script.js
+++ kuy/script/script.js
@@ -5,28 +5,30 @@ $(document).ready(function () {
         onLeave: function (link, index) {
             if (index == 1) {
                 $('.bottle-block').css({
-                    'transform': 'translateX(215%) translateY(0%) scale(3)'
+                    'transform': 'translateX(215%) translateY(10%) scale(2.4)'
                 });
+                $('.water-block').fadeOut(300);
+
             } else if (index == 2) {
                 $('.bottle-block').css({
                     'transform': 'translateX(190%) translateY(-20%) scale(1.2)',
                 });
+                $('.water-block').fadeOut(300);
+
             } else if (index == 3) {
                 $('.bottle-block').css({
                     'transform': 'translateX(80%) translateY(-5%) scale(1.2)'
                 });
+                $('.water-block').fadeOut(300);
+
             } else if (index == 4) {
                 $('.bottle-block').css({
-                    'transform': 'translateX(125%) translateY(80%) scale(1)'
+                    'transform': 'translateX(113%) translateY(80%) scale(1)',
+                    
                 });
+
+                $('.water-block').delay(900).fadeIn(900);
             }
         },
-        //        afterLoad: function (link,index) {
-        //            if (index == 1) {
-        //                $('.bottle-block').css({'right':'0px'});
-        //            } else if (index == 2) {
-        //                $('.bottle-block').css({'right':'50px'});
-        //            }
-        //        }
     });
 });
